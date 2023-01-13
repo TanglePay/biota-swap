9ed2fd9ea4a984459eade9eb64fcd899af0e60ab
TpSmpc <wyhx04@gmail.com>
2023-01-14 00:03:00 +0800

    modify accept

--- server/accept.go
+++ server/accept.go
@@ -21,8 +21,9 @@ func Accept() {
 			}
 			for i := range infoDatas {
 				d := infoDatas[i]
+				var agree bool = true
 				//validite the msgContext
-				for i, msg := range d.MsgContext {
+				for j, msg := range d.MsgContext {
 					msgContext := MsgContext{}
 					if err := json.Unmarshal([]byte(msg), &msgContext); err != nil {
 						gl.OutLogger.Error("json.Unmarshal to MsgContext error. %v", err)
@@ -34,10 +35,27 @@ func Accept() {
 						gl.OutLogger.Error("token client have no. %s, %s", msgContext.SrcToken, msgContext.DestToken)
 					}
 					if msgContext.Method == "wrap" {
-						t2.ValiditeWrapTxData(common.Hex2Bytes(d.MsgHash[i]), msgContext.TxData)
+						if baseTx, err := t2.ValiditeWrapTxData(common.Hex2Bytes(d.MsgHash[j]), msgContext.TxData); err != nil {
+							agree = false
+							gl.OutLogger.Error("ValiditeWrapTxData error. %s, %s, %v", d.MsgHash[j], string(msgContext.TxData), err)
+						} else if err = t1.CheckTxData(baseTx.Txid, baseTx.To, baseTx.Amount); err != nil {
+							agree = false
+							gl.OutLogger.Error("CheckTxData error. %v : %v", baseTx, err)
+						}
 					} else if msgContext.Method == "unwrap" {
-						t1.ValiditeUnWrapTxData(common.Hex2Bytes(d.MsgHash[i]), msgContext.TxData)
+						if baseTx, err := t1.ValiditeUnWrapTxData(common.Hex2Bytes(d.MsgHash[j]), msgContext.TxData); err != nil {
+							agree = false
+							gl.OutLogger.Error("ValiditeWrapTxData error. %s, %s, %v", d.MsgHash[j], string(msgContext.TxData), err)
+						} else if err = t2.CheckTxData(baseTx.Txid, baseTx.To, baseTx.Amount); err != nil {
+							agree = false
+							gl.OutLogger.Error("CheckTxData error. %v : %v", baseTx, err)
+						}
 					}
+				}
+				if err = smpc.AcceptSign(d, agree); err != nil {
+					gl.OutLogger.Error("smpc.AcceptSign error. %v : %v", d, err)
+				} else {
+					gl.OutLogger.Info("Accept the info. ")
 				}
 			}
 		}

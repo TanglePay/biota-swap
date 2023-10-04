a22c725d7868d8bb0e7964a7ecacc4ec675636ad
TpSmpc <wyhx04@gmail.com>
2023-10-04 10:27:03 +0800

    update script

--- build/dealErrorTx.sh
+++ build/dealErrorTx.sh
@@ -11,5 +11,5 @@ if [ ! -f "./config/smpc_k" ];then
     exit
 fi
 
-./bdeal_err ETH sETH 0x090a8850aec72e4ce6a26a0d3144eb94e0587c11dc31a0f9b3bfdd158c7121ac
-./bdeal_err WBTC sBTC 0x2c65f1f6b0ac16c235238e72e5fe000ff62a1f215bd702a8bcd08fb95000edf7
+./bdeal_err ETH sETH 0x253447775ff379c6f72bbc2b4511a34ff9a50aa8636ad85e6e8eeee4ac31f494
+./bdeal_err WBTC sBTC 0x2b33595fd3d1c5a698d4a9042b3fc120d23c9ce1228e8c0ee43ff2d4ce4e4174
